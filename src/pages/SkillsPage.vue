e52863147f23c372037ba3767c4d92b2f704d946
Bryan Tejada <bryanenid97@gmail.com>
2019-06-11 20:57:40 -0400

    added some more icons

--- src/pages/SkillsPage.vue
+++ src/pages/SkillsPage.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="container" id="Skills">
-    <section>
+  <div id="Skills" class="container">
+    <section class="frontEnd">
       <h1>Front End</h1>
       <ul>
-        <li>Javascript</li>
+        <li>
+          <icon name="javascript" class="icon"></icon>Javascript
+        </li>
         <ul class="indent">
           <li>ES5, ES6, ES7, ES8</li>
           <li>Paradigms:</li>
@@ -12,125 +14,330 @@
             <li>Functional</li>
             <li>Procedural</li>
           </ul>
-          <li>Frameworks and Libraries:</li>
+          <li>Frameworks/Lib:</li>
 
           <ul class="indent">
-            <li>Vue.js, Vuex</li>
-            <li>JQuery</li>
+            <li>
+              <icon name="vue" class="icon"></icon>Vue.js, Vuex
+            </li>
+            <li>
+              <icon name="jquery" class="icon"></icon>JQuery
+            </li>
             <li>Anime.js</li>
-            <li>p5.js</li>
-            <li>Anime.js</li>
+            <li>
+              <icon name="p5" class="icon"></icon>p5.js
+            </li>
             <li>Three.js</li>
+            <li>and more ...</li>
           </ul>
         </ul>
-        <li>CSS</li>
+        <br>
+        <br>
+        <li>
+          <icon name="css" class="icon"></icon>CSS3
+        </li>
         <ul class="indent">
           <li>Preprocessors:</li>
           <ul class="indent">
-            <li>LESS</li>
-            <li>SASS</li>
+            <li>
+              <icon name="less" class="icon"></icon>LESS
+            </li>
+            <li>
+              <icon name="sass" class="icon"></icon>SASS
+            </li>
           </ul>
           <li>Frameworks:</li>
           <ul class="indent">
-            <li>Bulma</li>
-            <li>Bootstrap / vue-bootstrap</li>
-            <li>Vuetify</li>
+            <li>
+              <icon name="bulma" class="icon"></icon>Bulma
+            </li>
+            <li>
+              <icon name="bootstrap" class="icon"></icon>Bootstrap
+            </li>
+            <li>
+              <icon name="vuetify" class="icon"></icon>Vuetify
+            </li>
           </ul>
         </ul>
-        <li>HTML5</li>
+        <br>
+        <br>
+        <li>
+          <icon name="html" class="icon"></icon>HTML5
+        </li>
         <ul class="indent">
           <li>WebGL</li>
           <li>Local Storage</li>
-          <li>SEO HTML-Tags and best practices</li>
-          <li>Data attr., REGEX, etc...</li>
+          <li>SEO Best Practices</li>
+          <li>Data attr.</li>
+          <li>REGEX</li>
+        </ul>
+        <br>
+      </ul>
+    </section>
+
+    <section class="backEnd">
+      <h2>Back End</h2>
+      <ul>
+        <li>
+          <icon name="node" class="icon"></icon>Node.js
+        </li>
+        <ul class="indent">
+          <li>REST:</li>
+          <ul class="indent">
+            <li>Express.js</li>
+            <li>Sails.js</li>
+            <li>Feather.js</li>
+          </ul>
+          <li>Web Sockets</li>
+          <ul class="indent">
+            <li>Socket.io</li>
+          </ul>
+          <li>Server Side Rendering</li>
+          <ul class="indent">
+            <li>Nuxt.js</li>
+          </ul>
         </ul>
       </ul>
     </section>
 
-    <section class="backend">
-      <div>
-        <h2>Back End</h2>
-        <ul>
-          <li>Node.js</li>
-          <ul class="indent">
-            <li>REST:</li>
-            <ul class="indent">
-              <li>Express.js</li>
-              <li>Sails.js</li>
-              <li>Feather.js</li>
-            </ul>
-            <li>Web Sockets:</li>
-            <ul class="indent">
-              <li>Socket.io</li>
-            </ul>
-            <li>Front End Server Side Rendering</li>
-            <ul class="indent">
-              <li>Nuxt.js</li>
-            </ul>
-          </ul>
+    <section class="database">
+      <h2>DataBases</h2>
+      <ul>
+        <li>NoSQL:</li>
+        <ul class="indent">
+          <li>MongoDB</li>
+          <li>Cassandra</li>
         </ul>
-      </div>
-
-      <div>
-        <h2>DataBases</h2>
-        <ul>
-          <li>NoSQL:</li>
-          <ul class="indent">
-            <li>MongoDB</li>
-            <li>Cassandra</li>
-          </ul>
-          <li>SQL:</li>
-          <ul class="indent">
-            <li>MySQL</li>
-            <li>PostgreSQL</li>
-            <li>SQLite</li>
-          </ul>
-          <li>DBaas:</li>
-          <ul class="indent">
-            <li>Google Cloud</li>
-            <li>Firebase</li>
-          </ul>
+        <li>SQL:</li>
+        <ul class="indent">
+          <li>
+            <icon name="mysql" class="icon"></icon>MySQL
+          </li>
+          <li>
+            <icon name="postgresql" class="icon"></icon>PostgreSQL
+          </li>
+          <li>SQLite</li>
         </ul>
-      </div>
+        <li>DBaas:</li>
+        <ul class="indent">
+          <li>
+            <icon name="googlecloud" class="icon"></icon>Google Cloud
+          </li>
+          <li>
+            <icon name="firebase" class="icon"></icon>Firebase
+          </li>
+        </ul>
+      </ul>
     </section>
   </div>
 </template>
 
 <script>
-export default {};
+import icon from "../components/AppIcons.vue";
+
+import AwesomeButton from "../components/LandingPageButton.vue";
+
+export default {
+  components: {
+    icon,
+    AwesomeButton
+  }
+};
 </script>
 
 <style lang="less" scoped>
+.icon {
+  float: left;
+  font-size: 11px;
+  margin-top: 1px;
+  height: 15px;
+  width: 18px;
+}
+
 .container {
-  color: white;
+  color: #747474;
+  background: #ededed;
   height: 100vh;
   display: grid;
-  grid-template-columns: repeat(2, 2fr);
-  justify-items: center;
-  align-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+  grid-gap: 100px;
+  // justify-items: center;
+  // align-items: center;
+}
+
+.container::before {
+  content: "Skills";
+  font-size: 50px;
+  size: 131.31px;
+  position: absolute;
+  left: 50%;
+  margin-left: -65.65px;
+  margin-top: 23px;
 }
 
 section {
-  display: grid;
-  border: 1px solid white;
   border-radius: 6px;
-  height: 600px;
+  padding: 40px 0 0 50px;
+  background: white;
+}
+.frontEnd {
+  grid-row: span 2 / auto;
+  margin: 100px 0 90px 100px;
+}
 
-  > div {
-    border: 1px solid white;
-    display: grid;
-  }
+.backEnd {
+  margin: 100px 100px 0 0;
+}
+.database {
+  margin: 0 100px 90px 0;
 }
 
 ul {
   margin: 20px;
   list-style: none;
 }
+
 .indent {
   padding: 0px;
-  margin: 0 0 0 20px;
+  margin: 10px 0 10px 50px;
   li {
     margin: 0px;
+  }
+}
+
+@tablet-down: ~"(max-width: 767px)";
+
+@media screen and (max-width: 1100px) {
+  .container {
+    height: initial;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: initial;
+    grid-gap: 10px;
+    // justify-items: center;
+    // align-items: center;
+  }
+
+  .frontEnd {
+    grid-row: span 2 / auto;
+    margin: 100px 0 100px 30px;
+  }
+
+  .backEnd {
+    margin: 100px 30px 0 0;
+  }
+  .database {
+    margin: 0 30px 100px 0;
+  }
+
+  .indent {
+    padding: 0px;
+    margin: 10px 0 10px 50px;
+    li {
+      margin: 0px;
+    }
+  }
+}
+
+@media screen and (max-height: 950px) and (min-width: 1100px) and (max-width: 1700px) {
+  .container {
+    height: initial;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: initial;
+    grid-gap: 100px;
+    // justify-items: center;
+    // align-items: center;
+  }
+
+  .frontEnd {
+    grid-row: span 2 / auto;
+    margin: 100px 0 100px 100px;
+  }
+
+  .backEnd {
+    margin: 100px 100px 0 0;
+  }
+  .database {
+    margin: 0 100px 100px 0;
+  }
+
+  .indent {
+    padding: 0px;
+    margin: 10px 0 10px 50px;
+    li {
+      margin: 0px;
+    }
+  }
+}
+
+@media (max-height: 950px) and (min-width: 1100px) {
+  .container {
+    height: initial;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: initial;
+    grid-gap: 100px;
+    // justify-items: center;
+    // align-items: center;
+  }
+
+  .frontEnd {
+    grid-row: span 2 / auto;
+    margin: 100px 0 100px 100px;
+  }
+
+  .backEnd {
+    margin: 100px 100px 0 0;
+  }
+  .database {
+    margin: 0 100px 100px 0;
+  }
+
+  .indent {
+    padding: 0px;
+    margin: 10px 0 10px 50px;
+    li {
+      margin: 0px;
+    }
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .container {
+    height: initial;
+    grid-template-columns: 1fr;
+    grid-auto-rows: initial;
+    grid-gap: 10px;
+    // justify-items: center;
+    // align-items: center;
+  }
+
+  section {
+    padding-bottom: 30px;
+  }
+
+  .frontEnd {
+    grid-row: span 2 / auto;
+    margin: 100px 10vw 0 10vw;
+  }
+
+  .backEnd {
+    margin: 0 10vw;
+  }
+  .database {
+    margin: 0 10vw 100px 10vw;
+  }
+
+  ul {
+    margin: initial;
+    list-style: none;
+  }
+
+  .indent {
+    padding: 0px;
+    margin: 5px 0 5px 25px;
+    li {
+      margin: 0px;
+    }
   }
 }
 </style>
