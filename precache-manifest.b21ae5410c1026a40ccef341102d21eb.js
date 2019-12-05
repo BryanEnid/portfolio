b86da2918c1280e29cbff0244445384f6f50668c
Bryan Enid <30734480+BryanEnid@users.noreply.github.com>
2019-12-05 01:25:17 -0500

    Updates

---
+++ precache-manifest.b21ae5410c1026a40ccef341102d21eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270e011a2411cdf57c1d",
+    "url": "css/app.ad2e5b2e.css"
+  },
+  {
+    "revision": "44c159da93ca126b2176fcdf5905eb87",
+    "url": "img/Bryan.44c159da.png"
+  },
+  {
+    "revision": "d43247da3ebd21f91353dab8147df1c3",
+    "url": "img/ach1.d43247da.jpg"
+  },
+  {
+    "revision": "a5945d43297fb4efc61083dba864f3fb",
+    "url": "img/ach2.a5945d43.png"
+  },
+  {
+    "revision": "0736137dd18dd49c0ab0de4442ca9de0",
+    "url": "img/instProfile.0736137d.jpg"
+  },
+  {
+    "revision": "14102fc40121abe91b2c255fd4fcc260",
+    "url": "img/project1.14102fc4.png"
+  },
+  {
+    "revision": "79bcbee1495edcacd3e27bebaaffe1f1",
+    "url": "img/roundedProfile.79bcbee1.png"
+  },
+  {
+    "revision": "561ec948c1435a7f834fb3292ca119d7",
+    "url": "index.html"
+  },
+  {
+    "revision": "270e011a2411cdf57c1d",
+    "url": "js/app.4e9e88e1.js"
+  },
+  {
+    "revision": "0029b23f79328484f317",
+    "url": "js/chunk-vendors.d3406a9e.js"
+  },
+  {
+    "revision": "fdd3af9b1a37beafcb0b6cfe223b3f63",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
